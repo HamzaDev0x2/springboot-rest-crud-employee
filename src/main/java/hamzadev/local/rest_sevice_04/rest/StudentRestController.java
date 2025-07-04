dd0f0068c56d6b6680452836cc5c42d5b9386ed8
Hamza_Dev <hamzadev232@gmail.com>
2025-07-04 17:48:58 +0100

    add a avariable endpoint based on student id

--- src/main/java/hamzadev/local/rest_sevice_04/rest/StudentRestController.java
+++ src/main/java/hamzadev/local/rest_sevice_04/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package hamzadev.local.rest_sevice_04.rest;
 
 import hamzadev.local.rest_sevice_04.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
@@ -11,15 +13,30 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> students;
+
+    @PostConstruct
+    public void init() {
+        students = new ArrayList<Student>();
+        System.out.println("Array list of students created");
+        students.add(new Student("ABBASSI", "Hamza"));
+        students.add(new Student("TAQI", "Abderrahim"));
+        students.add(new Student("LEO", "Messi"));
+        System.out.println("3 students added");
+
+    }
+
     //define endpoint for /students - returns a list of students
     @GetMapping("/students")
-    public List<Student> getStudents()
-    {
-        List<Student> students=new  ArrayList<Student>();
-        students.add(new Student("ABBASSI","Hamza"));
-        students.add(new Student("TAQI","Abderrahim"));
-        students.add(new Student("LEO","Messi"));
-
+    public List<Student> getStudents() {
         return students;
     }
+
+    //define endpoint for /students/{idStudent} - returns a student based on his list index
+    @GetMapping("/students/{studentId}")
+    public Student getStudents(@PathVariable int studentId) {
+
+        return students.get(studentId);
+    }
 }
+

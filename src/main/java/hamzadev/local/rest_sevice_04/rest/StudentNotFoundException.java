8f662538793ea31a136793e77b535bd6cde829b6
Hamza_Dev <hamzadev232@gmail.com>
2025-07-04 16:49:00 +0100

    index out of bounds exception handling

---
+++ src/main/java/hamzadev/local/rest_sevice_04/rest/StudentNotFoundException.java
@@ -0,0 +1,15 @@
+package hamzadev.local.rest_sevice_04.rest;
+
+public class StudentNotFoundException extends RuntimeException{
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/hamzadev/local/rest_sevice_04/rest/StudentRestController.java
+++ src/main/java/hamzadev/local/rest_sevice_04/rest/StudentRestController.java
@@ -1,9 +1,11 @@
 package hamzadev.local.rest_sevice_04.rest;
 
 import hamzadev.local.rest_sevice_04.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,15 +13,58 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private  List<Student> students;
+
+    @PostConstruct
+    public void init()
+    {
+        students=new  ArrayList<Student>();
+        System.out.println("Array list of students created");
+        students.add(new Student("ABBASSI","Hamza"));
+        students.add(new Student("TAQI","Abderrahim"));
+        students.add(new Student("LEO","Messi"));
+        System.out.println("3 students added");
+
+    }
+
     //define endpoint for /students - returns a list of students
     @GetMapping("/students")
     public List<Student> getStudents()
     {
-        List<Student> students=new  ArrayList<Student>();
-        students.add(new Student("ABBASSI","Hamza"));
-        students.add(new Student("TAQI","Abderrahim"));
-        students.add(new Student("LEO","Messi"));
-
         return students;
     }
+    //define endpoint for /students/{idStudent} - returns a student based on his list index
+    @GetMapping("/students/{studentId}")
+    public Student getStudents(@PathVariable int studentId)
+    {
+        //check student id against list size
+        if(students.size()<=studentId || studentId<0)
+            throw new StudentNotFoundException("Student id not found : "+ studentId);
+        return students.get(studentId);
+    }
+
+    //add exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc)
+    {
+        //create a student error response
+        StudentErrorResponse response=new StudentErrorResponse();
+
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setMessage(exc.getMessage());
+        response.setTimeStamp(System.currentTimeMillis());
+
+        //Return a response entity
+        return new ResponseEntity<StudentErrorResponse>(response, HttpStatus.NOT_FOUND);
+    }
 }
+
+
+
+
+
+
+
+
+
+

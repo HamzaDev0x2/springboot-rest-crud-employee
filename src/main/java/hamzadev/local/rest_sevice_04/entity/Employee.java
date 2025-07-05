c71be34c6b235584683d93bbb670a3cd25c08faa
Hamza_Dev <hamzadev232@gmail.com>
2025-07-05 17:52:56 +0100

    add employee entity

---
+++ src/main/java/hamzadev/local/rest_sevice_04/entity/Employee.java
@@ -0,0 +1,79 @@
+package hamzadev.local.rest_sevice_04.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="emplyee")
+public class Employee {
+
+    //define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    //define constructors
+    public Employee() {}
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    //define getters and setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    //define toString method
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}

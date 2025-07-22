deac329348aaf80ebae8c7916b09b2ad57182657
Hamza_Dev <hamzadev232@gmail.com>
2025-07-22 03:25:11 +0100

    add spring data rest configurations

--- src/main/java/hamzadev/local/rest_sevice_04/dao/EmployeeRepository.java
+++ src/main/java/hamzadev/local/rest_sevice_04/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package hamzadev.local.rest_sevice_04.dao;
 
 import hamzadev.local.rest_sevice_04.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     // thats it ... no need to write any code for basic crud methods
 }
